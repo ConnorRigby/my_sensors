e084070ca312539f3b3e5cd94a13f95cdc9bed7d
Connor Rigby <connor@farmbot.io>
2017-12-26 20:27:54 -0800

    Use rc version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MySensors.Mixfile do
   def project do
     [
       app: :my_sensors,
-      version: "0.1.0",
+      version: "0.1.0-rc1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
